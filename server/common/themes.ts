380f7240f3dc376685f1746c2174063d8d062613
dubisdev <dubisdev@gmail.com>
2023-03-06 19:11:00 +0100

    chore: use unique url for themes

--- server/common/themes.ts
+++ server/common/themes.ts
@@ -1,14 +1,10 @@
-const prefix = process.env.NODE_ENV !== "production"
-  ? process.env.VITE_DEV_SERVER_URL
-  : `.`;
-
 export const themes = [
   {
-    value: `${prefix}/themes/light.css`,
+    value: `./themes/light.css`,
     label: "Light",
   },
   {
-    value: `${prefix}/themes/dark.css`,
+    value: `./themes/dark.css`,
     label: "Dark",
   },
 ] as const;
