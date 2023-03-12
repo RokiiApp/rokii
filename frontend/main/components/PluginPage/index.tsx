3137ba38f8c5c3b19d68bacd4b59ef38f5d15d88
dubisdev <dubisdev@gmail.com>
2023-03-12 23:46:55 +0100

    chore: add pluginpage component

---
+++ frontend/main/components/PluginPage/index.tsx
@@ -0,0 +1,10 @@
+import { RouteComponentProps } from "wouter";
+
+export const PluginPage = ({ params }: RouteComponentProps) => {
+    const pluginName = params.plugin;
+    return (
+        <div>
+            {pluginName}
+        </div>
+    );
+};
