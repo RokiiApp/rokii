a27f7b7607f22d809b2de93618bb545deeeb75d3
dubisdev <dubisdev@gmail.com>
2023-03-20 18:36:18 +0100

    chore: improve status bar

--- frontend/main/components/StatusBar/index.tsx
+++ frontend/main/components/StatusBar/index.tsx
@@ -1,10 +1,38 @@
-import { useRokiStore } from '@/state/rokiStore';
+import { CHANNELS, StatusBarState } from 'common/constants/events';
+import { on } from 'common/ipc';
+import { ipcRenderer } from 'electron';
+import { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 
 function StatusBar () {
-  const statusBarText = useRokiStore((s) => s.statusBarText);
+  const [statusBarText, setStatusBarText] = useState('');
+  const [icon, setIcon] = useState<StatusBarState['icon']>(null);
+  const [timeoutValue, setTimeoutValue] = useState<StatusBarState['timeout']>(null);
+
+  useEffect(() => {
+    on(CHANNELS.StatusBarUpdate, (_, state) => {
+      setStatusBarText(state.statusBarText);
+      setIcon(state.icon);
+      setTimeoutValue(state.timeout);
+    });
+
+    return () => {
+      ipcRenderer.removeAllListeners(CHANNELS.StatusBarUpdate);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (timeoutValue) {
+      setTimeout(() => {
+        setStatusBarText('');
+        setIcon(null);
+        setTimeoutValue(null);
+      }, timeoutValue);
+    }
+  }, [timeoutValue]);
+
   if (!statusBarText) return null;
-  return <div className={styles.statusBar}>{statusBarText}</div>;
+  return <div className={styles.statusBar}>{icon}{statusBarText}</div>;
 }
 
 export { StatusBar };
